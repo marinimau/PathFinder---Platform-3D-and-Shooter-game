e7b704186831653ffd3f74d72b8ce37fb046703b
Mauro Marini <mauro.marini97@gmail.com>
2019-04-09 18:56:47 +0200

    magazine gui nuova

--- Assets/scripts/Gun/magazineController.cs
+++ Assets/scripts/Gun/magazineController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class magazineController : MonoBehaviour
 {
-    static float startWidth = 350;
+    static float startWidth = 167.89f;
     float currentWidth = startWidth;
     RectTransform rectTransform;
     // Start is called before the first frame update
@@ -12,7 +12,7 @@ public class magazineController : MonoBehaviour
     {
         rectTransform = GetComponent<RectTransform>();
         currentWidth = (startWidth / (14)) * (GunScript.nColpi+1);
-        rectTransform.sizeDelta = new Vector2(currentWidth, 50);
+        rectTransform.sizeDelta = new Vector2(currentWidth, 30.06f);
     }
 
     // Update is called once per frame
@@ -20,14 +20,14 @@ public class magazineController : MonoBehaviour
     {
         if(Input.GetButtonDown("Fire1") && !GunScript.armaScarica){
             currentWidth =(startWidth/(14))*GunScript.nColpi;
-            rectTransform.sizeDelta = new Vector2(currentWidth, 50);
+            rectTransform.sizeDelta = new Vector2(currentWidth, 30.06f);
 
         }
 
         if(Input.GetKey(KeyCode.R)){
             //non uso startWidth per gestire i due casi di ricarica
             currentWidth = (startWidth / (14)) * (GunScript.nColpi+1);
-            rectTransform.sizeDelta = new Vector2(currentWidth, 50);
+            rectTransform.sizeDelta = new Vector2(currentWidth, 30.06f);
         }
     }
 }
