f274e5381b3b25adad534c836605fbd5962db5b3
Mauro Marini <mauro.marini97@gmail.com>
2019-04-12 19:59:33 +0200

    Patrol fixed, Trigger coni
    nemico rispetta gravità, compie movimenti realistici simili a quelli che può compiere il player, rileva quando siamo nel suo campo visivo

--- Assets/scripts/Enemy/EnemySight.cs
+++ Assets/scripts/Enemy/EnemySight.cs
@@ -2,23 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*---------------------------
+ *  cono da lunga distanza, funziona solo con fonte luminosa
+ * --------------------------*/
 public class EnemySight : MonoBehaviour
 {
-    GameObject cone;
+
+    public bool player_contact;
+    public bool light;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        player_contact = false;
+        light = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        void OnTriggerEnter(Collider other)
-        {
-            Debug.Log(gameObject.name + "triggerata da" + other.gameObject.name);
+        if(player_contact && light){
+            Show_stealth_status.icon = 2;
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //player
+        if(other.gameObject.name=="thug1"){
+            player_contact = true;
+            Debug.Log(" player_contact " + player_contact);
+        }
+        //luce
+        if (other.gameObject.name == "light")
+        {
+            light = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "thug1")
+        {
+            player_contact = false;
+            Debug.Log(" player_contact " + player_contact);
+
+        }
+
+        if (other.gameObject.name == "light")
+        {
+            light = false;
+        }
+
+
     }
 
 
--- Assets/scripts/Enemy/Patrol.cs
+++ Assets/scripts/Enemy/Patrol.cs
@@ -1,48 +1,104 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Patrol : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 1;
     public Transform[] moveSpots;
     private int randomSpots;
 
     private float waitTime;
-    public float startWaitTime;
+    public float startWaitTime=5;
 
+    public bool contact_status=false;
+    public float gravity = -12;
+    public NavMeshAgent navMesh;
+    public GameObject player;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        /*------------------------
+         *  inizializzo la navmesh
+         * -----------------------*/
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.speed = speed;
+        navMesh.autoBraking = false;
         waitTime = startWaitTime;
+        navMesh.updateRotation = false;
+
+
+        /*------------------------
+         *  seleziono a caso il primo punto del giro di pattuglia
+         * -----------------------*/
         randomSpots = Random.Range(0, moveSpots.Length);
 
         
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpots].position, speed * Time.deltaTime);
-        //si muove dalla posizione a una posizione random con una certa velocità
-
-
-        if (Vector3.Distance(transform.position, moveSpots[randomSpots].position) < 0.2f)
+        if(!contact_status)
         {
-            if (waitTime <= 0)
+                /*------------------------
+                 *  se il nemico non ci vede
+                 * -----------------------*/
+
+
+            if (!navMesh.pathPending && navMesh.remainingDistance < 0.5f)
             {
-                randomSpots = Random.Range(0, moveSpots.Length);
-                waitTime = startWaitTime;
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
+                /*------------------------
+                 *  se è in posizione
+                 * -----------------------*/
+                if (waitTime <= 0)
+                {
+                    /*------------------------
+                     *  vai alla prossima posizione
+                     * -----------------------*/
+                    //randomSpots =(Random.Range(0, moveSpots.Length);
+                    randomSpots = (randomSpots + 1) % moveSpots.Length;
+                    waitTime = startWaitTime;
+                    navMesh.SetDestination(moveSpots[randomSpots].position);
+
+                }
+                else
+                {
+                    /*------------------------
+                     *  aspetta nel waypoint
+                     * -----------------------*/
+                    waitTime -= Time.deltaTime;
+                    //qua deve guardarsi attorno
+                }
+
+            } else{
+                /*------------------------
+                 *  cambio di posizione
+                 * -----------------------*/
+
+                navMesh.destination = moveSpots[randomSpots].position;
+                if (navMesh.velocity.sqrMagnitude > Mathf.Epsilon)
+                {
+                    transform.rotation = Quaternion.LookRotation(navMesh.velocity.normalized);
+                }
+
+                //navMesh.transform.LookAt(moveSpots[randomSpots].position);
             }
 
+        } else {
+
+            /*------------------------
+             *  se siamo stati visti dal nemico
+             * -----------------------*/
+            navMesh.destination = player.transform.position;
+            transform.LookAt(player.transform.position);
         }
+
 
     }
 }
