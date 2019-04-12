2ad9f973b5a26fa4da96c9d8add08caddaa08532
Marcello <marcy.agnese@gmail.com>
2019-04-12 15:41:40 +0200

    fix cono

---
+++ Assets/scripts/Enemy/EnemySight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    GameObject cone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(gameObject.name + "triggerata da" + other.gameObject.name);
+        }
+    }
+
+
+}
