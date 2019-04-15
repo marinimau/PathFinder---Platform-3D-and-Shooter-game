7cc14252ad8e210b4bc2163c05e9f0cff92e3c18
Marcello <marcy.agnese@gmail.com>
2019-04-15 11:24:53 +0200

    fix completo del menu

--- Assets/SlimUI/Modern Menu 1/Scripts/PauseMenu.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/PauseMenu.cs
@@ -7,20 +7,36 @@ public class PauseMenu : MonoBehaviour
 
 
     [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] public GameObject cameraObj;
     [SerializeField] private bool isPaused;
+    Animator CameraObject;
+    public GameObject riprendiBtn;
+    public GameObject salvaBtn;
+    public GameObject menuPrincipaleBtn;
+    public GameObject areYouSure;
+
+    void Start()
+    {
+        CameraObject = transform.GetComponent<Animator>();
+        cameraObj.SetActive(false);
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             isPaused = !isPaused;
+            areYouSure.gameObject.SetActive(false);
         }
         if (isPaused)
         {
             ActivateMenu();
+
         }
         else
         {
             DeactivateMenu();
+            cameraObj.SetActive(false);
         }
     }
 
@@ -29,6 +45,8 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0; //freeze del tempo all'attivazione del menu
         AudioListener.pause = true;
         pauseMenuUI.SetActive(true);
+        cameraObj.SetActive(true);
+
     }
     public void DeactivateMenu()
     {
@@ -36,5 +54,45 @@ public class PauseMenu : MonoBehaviour
         AudioListener.pause = false;
         pauseMenuUI.SetActive(false);
         isPaused = false;
+    }
+
+   
+    public void PlayCampaign()
+    {
+        areYouSure.gameObject.SetActive(false);
+        riprendiBtn.gameObject.SetActive(true);
+        salvaBtn.gameObject.SetActive(true);
+        menuPrincipaleBtn.gameObject.SetActive(true);
+    }
+    public void DisablePlayCampaign()
+    {
+        riprendiBtn.gameObject.SetActive(true);
+        salvaBtn.gameObject.SetActive(true);
+        menuPrincipaleBtn.gameObject.SetActive(true);
+    }
+    public void Position1()
+    {
+        CameraObject.SetFloat("Animate", 0);
+    }
+    public void Position2()
+    {
+        CameraObject.SetFloat("Animate", 1);
+    }
+
+    // Are You Sure - Quit Panel Pop Up
+    public void AreYouSure()
+    {
+        areYouSure.gameObject.SetActive(true);
+        DisablePlayCampaign();
+    }
+
+    public void No()
+    {
+        areYouSure.gameObject.SetActive(false);
+    }
+
+    public void Yes()
+    {
+        Application.Quit();
     }
 }
