55633a130dd6350d7a374cf6810c2bab30e99946
Marcello <marcy.agnese@gmail.com>
2019-04-23 18:37:08 +0200

    aggiunta di playground per testare il menu

---
+++ Assets/scripts/Save_Load/CharacterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : MonoBehaviour
+{
+        public string characterName;
+        public float power;
+        public int bullets;
+}
---
+++ Assets/scripts/Save_Load/PlayerPrefsCharacterSaver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsCharacterSaver : MonoBehaviour
+{
+    public CharacterData characterData;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            SaveCharacter(characterData, 0);
+
+        if (Input.GetKeyDown(KeyCode.L))
+            characterData = LoadCharacter(0);
+    }
+
+    static void SaveCharacter(CharacterData data, int characterSlot)
+    {
+        /*PlayerPrefs.SetString("characterName_CharacterSlot" + characterSlot, data.characterName);
+        PlayerPrefs.SetFloat("power_CharacterSlot" + characterSlot, data.power);
+        PlayerPrefs.SetInt("bullets_CharacterSlot" + characterSlot, data.bullets);*/
+        PlayerPrefs.SetInt("bullets_CharacterSlot" + characterSlot, data.bullets);
+        PlayerPrefs.Save();
+    }
+
+    static CharacterData LoadCharacter(int characterSlot)
+    {
+        CharacterData loadedCharacter = new CharacterData();
+        /*loadedCharacter.characterName = PlayerPrefs.GetString("characterName_CharacterSlot" + characterSlot);
+        loadedCharacter.power = PlayerPrefs.GetFloat("power_CharacterSlot" + characterSlot);
+        loadedCharacter.bullets = PlayerPrefs.GetInt("bullets_CharacterSlot" + characterSlot);*/
+
+        loadedCharacter.bullets = PlayerPrefs.GetInt("bullets_CharacterSlot" + characterSlot);
+
+        return loadedCharacter;
+    }
+}
