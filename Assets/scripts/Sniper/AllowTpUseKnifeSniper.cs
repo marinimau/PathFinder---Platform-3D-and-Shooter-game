7ae035c5c006aa1c45fbfdcc9763690fc4235e8a
Mauro Marini <mauro.marini97@gmail.com>
2019-04-27 19:10:26 +0200

    cecchino

--- Assets/scripts/Sniper/AllowTpUseKnifeSniper.cs
+++ Assets/scripts/Sniper/AllowTpUseKnifeSniper.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class AllowTpUseKnifeSniper : MonoBehaviour
 {
+    public GameObject sniper;
+    bool allowKnife;
+    public GameObject knife;
+    public GameObject player;
+    Animator anim;
+    CharacterControllerScript controller;
+    Sniper sniperController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = player.GetComponent<Animator>();
+        allowKnife = false;
+        controller = player.GetComponent<CharacterControllerScript>();
+        sniperController = sniper.GetComponent<Sniper>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (allowKnife)
+        {
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                StartCoroutine(OnAnimationComplete());
+            }
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //player
+        if (other.gameObject.tag.Equals("Player") && !sniperController.isDead && !CharacterControllerScript.isDead)
+        {
+            allowKnife = true;
+            ShowMessage.id = 1;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            allowKnife = false;
+            ShowMessage.id = 0;
+        }
+
+    }
+
+
+    void deactivateKinife()
+    {
+        knife.SetActive(false);
+    }
+
+    IEnumerator OnAnimationComplete()
+    {
+        sniperController.stopEnemy();
+        float knife_waittime = 1.00f;
+        anim.SetBool("stabbing", true);
+        knife.SetActive(true);
+        controller.standStill();
+        yield return new WaitForSeconds(knife_waittime);
+        anim.SetBool("stabbing", false);
+        knife.SetActive(false);
+        sniperController.kill();
+        controller.setStandardWalkSpeed();
+        yield return true;
+    }
+
 }
+
---
+++ Assets/scripts/Sniper/Sniper.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Sniper : MonoBehaviour
+{
+    public float speed = 1;
+    public Transform[] moveSpots;
+    private int randomSpots;
+
+    private float waitTime;
+    private bool setWait;
+    public float startWaitTime = 1;
+
+    public float gravity = -12;
+    public NavMeshAgent navMesh;
+    public GameObject player;
+    private Animator animEnemy;
+    public bool isLamabile;
+    public GameObject enemy;
+    public ParticleSystem blood;
+    public ParticleSystem bloodBody;
+
+    public ParticleSystem fire;
+
+    public bool isFiring;
+    public float fireTimer;
+
+    public float life;
+    public bool isDead;
+    public bool killOk;
+
+
+    public float cadenzaFuoco = 1f;
+
+    public AudioSource sniperFireSound;
+
+    public GameObject zonaLama;
+
+    public AudioSource hitSound;
+
+    public bool bodyHit;
+    public bool headHit;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*------------------------
+         *  inizializzo la navmesh
+         * -----------------------*/
+        navMesh = GetComponent<NavMeshAgent>();
+        animEnemy = transform.GetComponent<Animator>();
+        navMesh.speed = speed;
+        navMesh.autoBraking = false;
+        waitTime = startWaitTime;
+        navMesh.updateRotation = false;
+        isLamabile = false;
+        isFiring = false;
+        isDead = false;
+        killOk = false;
+        //fuoco.enableEmission = false;
+        life = 100f;
+
+        zonaLama = gameObject.transform.GetChild(0).gameObject;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        //animEnemy = navMesh.gameObject.GetComponentInChildren<Animator>();
+        //animEnemy.SetBool("isWalking", true);
+        //animEnemy.SetFloat("speedPercentage", 1);
+        /*------------------------
+         *  seleziono a caso il primo punto del giro di pattuglia
+         * -----------------------*/
+        randomSpots = Random.Range(0, moveSpots.Length);
+
+        bodyHit = false;
+        headHit = false;
+
+    }
+
+    private void Update()
+    {
+        if (life == 0)
+            isDead = true;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (isDead && !killOk)
+        {
+            kill();
+        }
+
+
+        if (!CharacterControllerScript.player_contact)
+        {
+          /*-------------------------
+           *  se il cecchino non ci vede si guarda attorno
+           * -----------------------*/
+
+        }
+        else
+        {
+
+            /*------------------------
+             *  se siamo stati visti dal nemico
+             * -----------------------*/
+            transform.LookAt(player.transform.position + (new Vector3(0, 1f, 0)));
+            //spara
+            fireOnPlayer();
+
+        }
+
+        if (headHit)
+        {
+            blood.Play();
+            kill();
+
+        }
+
+        if (bodyHit)
+        {
+            bloodBody.Play();
+            if (life > 0)
+            {
+                decrLife(50);
+                hitSound.Play();
+            }
+            bodyHit = false;
+
+        }
+
+    }
+
+    void fireOnPlayer()
+    {
+        RaycastHit hit;
+        Vector3 fucile = navMesh.transform.position;
+        fucile.y += 0.5f;
+        if (!isFiring && CharacterControllerScript.player_contact)
+        {
+            isFiring = true;
+            fireTimer = Random.Range(0, 5);
+            if (Physics.Raycast(fucile, navMesh.transform.forward, out hit))
+            {
+                //animEnemy.SetBool("isShooting", true);
+                //fire.enableEmission = true;
+               
+                Debug.DrawRay(fucile, navMesh.transform.forward * 10, Color.green);
+                Debug.Log("Nemico colpisce: " + hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "Player" && !CharacterControllerScript.immortality)
+                {
+                    //fire.Play();
+                    sniperFireSound.Play();
+                    Debug.Log("Enemy Fire");
+                    CharacterControllerScript.decrHealth(100);
+                    CharacterControllerScript.PlayerBlood.Play();
+                    Debug.Log("Player hit by sniper");
+                    Talk.id = 2;
+                }
+            }
+        }
+        fireTimer -= Time.deltaTime * cadenzaFuoco;
+        if (fireTimer <= 0)
+        {
+            isFiring = false;
+        }
+    }
+
+    public void decrLife(int damage)
+    {
+        if (life - damage > 0)
+        {
+            life -= damage;
+        }
+        else
+        {
+            life = 0;
+            kill();
+        }
+
+    }
+
+    public void incLife(int cura)
+    {
+        if (life + cura <= 100)
+        {
+            life += cura;
+        }
+        else
+        {
+            life = 100;
+        }
+    }
+
+    public void kill()
+    {
+        ShowMessage.id = 0;
+        speed = 0;
+        if (animEnemy.GetBool("isHeadHit") == false)
+            animEnemy.SetBool("isDead", true);
+        Destroy(zonaLama);
+        Destroy(navMesh);
+        navMesh.enabled = false;
+        if (!isDead)
+        {
+            isDead = true;
+            killOk = true;
+        }
+
+        Destroy(this);
+    }
+
+
+    public void stopEnemy()
+    {
+        this.speed = 0f;
+        navMesh.isStopped = true;
+        animEnemy.SetFloat("speedPercentage", 0.1f);
+    }
+
+}
